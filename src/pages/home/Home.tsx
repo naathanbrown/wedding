34de72fe4701789a19f64cee208c6deef308e59c
naathanbrown <nathancjb@googlemail.com>
2025-02-23 14:07:06 +0000

    create home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,28 @@
+import { Link, useLocation } from "react-router";
 import { Navbar } from "../../components/navbar";
 import "./Home.css";
 
 export const Home = () => {
-  const userType = localStorage.getItem("guest_type");
+  const { search } = useLocation();
 
   return (
     <div className="App">
       <Navbar />
-      <p> your guest type is {userType}</p>
+      <div className="image-container">
+        <img id="header-image" src="floral_header4.jpg" alt="flowers" />
+      </div>
+      <h1 id="top-line">With love and joy,</h1>
+      <h1 id="middle-line">
+        Nathan <br /> and <br /> Samantha
+      </h1>
+      <h1 id="bottom-line">Invite you to celebrate their wedding</h1>
+      <h1 id="bottom-line">27 September 2025</h1>
+      <h1 id="bottom-line">
+        Venue and general information available from nav bar above
+      </h1>
+      <Link id="bottom-line-link" to={{ pathname: "/rsvp", search }}>
+        Please RSVP
+      </Link>
     </div>
   );
 };
